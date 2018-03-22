d0d8f6d6bf5ef425c01687cf20f005985c0e8e39
Dasith Wijes <dasith.wijes@readify.net>
2018-03-22 17:22:33 +1100

    Add project files.

---
+++ ActorClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Actors;
+using Microsoft.ServiceFabric.Actors.Client;
+using TestActor.Interfaces;
+
+namespace ActorClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            ITestActor actor = ActorProxy.Create<ITestActor>(ActorId.CreateRandom(), new Uri("fabric:/ServiceFabricTest/TestActorService"));
+            var retval = actor.GetCountAsync(new CancellationToken());
+            Console.Write(retval.Result);
+            Console.ReadLine();
+        }
+    }
+}
